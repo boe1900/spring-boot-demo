042901bca5b5d9035592a57d908a07c17be3b2fb
huabo <huabo1900@163.com>
2018-08-18 10:12:21 +0800

    Initial commit

---
+++ src/main/java/com/bobo/controller/TestController.java
@@ -0,0 +1,19 @@
+package com.bobo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author huabo
+ * @date 2018/8/18
+ */
+@RestController
+public class TestController {
+
+
+    @RequestMapping("/hello")
+    public String sayHello(){
+        return "hello";
+    }
+}
